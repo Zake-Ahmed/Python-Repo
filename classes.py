112a3bcec0bfd911192b51c0338d80ea29f3499e
Zake-Ahmed <zake3012@gmail.com>
2022-06-09 11:40:55 +0000

    classes

---
+++ classes.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, age, class_="Student"):
+        self.name=name
+        self.age=age
+        self.class_=class_
+    def score(self,test1,test2,test3):
+        print((test1+test2+test3)/3)
+        return (test1+test2+test3)/3
+
+
+zake = Student('zake','30')
+
+score=zake.score(80,43,39)
+print(getattr(zake,"class_"))
+
